b0d97c68050ed2dd452d664e60b5138d2bda793b
jvijt <info@vijt.be>
2026-08-28 18:38:30 +0200

    Add Garage Barnes plugin scaffold

---
+++ deploy/wp-content/plugins/garage-barnes/garage-barnes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Garage Barnes
+ * Description: Custom functionality for the Garage Barnes WordPress website.
+ * Version: 0.1.0
+ * Author: Garage Barnes
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Project bootstrap. Functionality will be added incrementally.
