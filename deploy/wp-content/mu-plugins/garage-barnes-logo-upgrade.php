acaabc0150d99f9374c817f38d8e5085ad4a1109
jvijt <info@vijt.be>
2026-09-13 21:00:56 +0200

    Use new high-resolution Barnes logos site-wide

---
+++ deploy/wp-content/mu-plugins/garage-barnes-logo-upgrade.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Garage Barnes - use the new high-resolution logo files everywhere on the public site.
+ */
+if (!defined('ABSPATH')) { exit; }
+
+function gb_new_logo_urls() {
+    return array(
+        'garage_black' => 'https://garagebarnes.be/wp-content/uploads/2026/09/Barnes-Garage-Logo-Zwart.png',
+        'garage_white' => 'https://garagebarnes.be/wp-content/uploads/2026/09/Barnes-Garage-Logo-Wit.png',
+        'towing_black' => 'https://garagebarnes.be/wp-content/uploads/2026/09/Barnes-Takeldienst-Logo-Zwart.png',
+        'towing_white' => 'https://garagebarnes.be/wp-content/uploads/2026/09/Barnes-Takeldienst-Logo-Wit.png',
+    );
+}
+
+/**
+ * Replace every legacy Barnes logo URL in rendered frontend HTML.
+ * This also covers older shortcodes/MU plugins that still point to the original plugin assets.
+ */
+function gb_replace_legacy_logo_urls($html) {
+    if (!is_string($html) || $html === '') { return $html; }
+
+    $new = gb_new_logo_urls();
+    $base = plugins_url('garage-barnes/assets/img/');
+
+    $search = array(
+        $base . 'garage-barnes-logo.png',
+        $base . 'garage-barnes-logo-wit.png',
+        $base . 'takeldienst-barnes-logo.png',
+        $base . 'takeldienst-barnes-logo-wit.png',
+        home_url('/wp-content/plugins/garage-barnes/assets/img/garage-barnes-logo.png'),
+        home_url('/wp-content/plugins/garage-barnes/assets/img/garage-barnes-logo-wit.png'),
+        home_url('/wp-content/plugins/garage-barnes/assets/img/takeldienst-barnes-logo.png'),
+        home_url('/wp-content/plugins/garage-barnes/assets/img/takeldienst-barnes-logo-wit.png'),
+    );
+
+    $replace = array(
+        $new['garage_black'],
+        $new['garage_white'],
+        $new['towing_black'],
+        $new['towing_white'],
+        $new['garage_black'],
+        $new['garage_white'],
+        $new['towing_black'],
+        $new['towing_white'],
+    );
+
+    return str_replace($search, $replace, $html);
+}
+
+add_action('template_redirect', function () {
+    if (is_admin() || wp_doing_ajax() || (defined('REST_REQUEST') && REST_REQUEST)) { return; }
+    ob_start('gb_replace_legacy_logo_urls');
+}, 1);
